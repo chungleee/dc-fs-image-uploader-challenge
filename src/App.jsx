d3c3869592540ec91a45778bdce89a63e8083e96
Leon Chung <leon.karho.chung@gmail.com>
2022-10-27 16:37:12 -0400

    import image uploader component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ImageUploader from "./components/ImageUploader/ImageUploader";
+import style from "./App.module.scss";
 
 const App = () => {
-	return <div>hello world</div>;
+	return (
+		<div className={style.app}>
+			<ImageUploader />
+		</div>
+	);
 };
 
 export default App;
