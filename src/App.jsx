09d11509a3e4bc9d4ce41a1aedf3b728c14fae66
Leon Chung <leon.karho.chung@gmail.com>
2022-10-31 15:34:37 -0400

    logic to load Uploading or Uploader components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageUploader from "./components/ImageUploader/ImageUploader";
 import style from "./App.module.scss";
+import Uploading from "./components/Uploading/Uploading";
 
 const App = () => {
+	const [is_uploading, set_is_uploading] = useState(false);
+	const handle_is_uploading = () => {
+		set_is_uploading(true);
+	};
 	return (
 		<div className={style.app}>
-			<ImageUploader />
+			{is_uploading ? (
+				<Uploading />
+			) : (
+				<ImageUploader handle_is_uploading={handle_is_uploading} />
+			)}
 		</div>
 	);
 };
