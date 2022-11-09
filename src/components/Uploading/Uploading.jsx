786cb8dc9d2b47a3e1532193d04392136b6f850f
Leon Chung <leon.karho.chung@gmail.com>
2022-11-09 15:52:09 -0500

    uploading component styling

--- src/components/Uploading/Uploading.jsx
+++ src/components/Uploading/Uploading.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Uploading.module.scss";
 
 const Uploading = () => {
-	return <div>uploading component</div>;
+	return (
+		<div className={styles.container}>
+			<div>
+				<h2>Uploading...</h2>
+				<div className={styles.progress_bar}></div>
+			</div>
+		</div>
+	);
 };
 
 export default Uploading;
